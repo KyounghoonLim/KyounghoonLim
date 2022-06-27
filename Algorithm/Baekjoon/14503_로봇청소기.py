e57c6126503af14f80f91c78fee5bd169a873c1a
LKH <mogies@naver.com>
2022-06-28 00:36:23 +0900

    [Algo]: Baekjoon 14503_로봇청소기 solved

--- Algorithm/Baekjoon/14503_로봇청소기.py
+++ Algorithm/Baekjoon/14503_로봇청소기.py
@@ -1,45 +1,78 @@
 import pprint
 
+
 di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 
 
-def can_go(i, j):
-    for t in range(4):
-        ti, tj = i + di[t], j + dj[t]
-        if test_case[ti][tj] == 0 or test_case[ti][tj] == 2:
-            return True
+def go_next(pos:list, t):
+    next_pos = pos.copy()
+    next_pos[2] = (pos[2] + t) % 4
+    next_pos[1] += dj[next_pos[2]]
+    next_pos[0] += di[next_pos[2]]
+    if can_go(next_pos):
+        return next_pos
+    else:
+        return False
+
+
+def go_back(pos:list):
+    d = (pos[2] + 2) % 4
+    back_pos = pos.copy()
+    back_pos[0] += di[d]
+    back_pos[1] += dj[d]
+    if can_back(back_pos):
+        return back_pos
+    else:
+        return False
+
+
+def can_go(pos):
+    ti, tj = pos[0], pos[1]
+    if 0 <= ti < N and 0 <= tj < M and test_case[ti][tj] == 0:
+        return True
     return False
 
 
-def run(pos, n=1):
-    global ans
+def can_back(pos):
+    ti, tj = pos[0], pos[1]
+    if 0 <= ti < N and 0 <= tj < M and test_case[ti][tj] != 1:
+        return True
+    return False
 
-    for i in range(1-n, n):
-        for j in range(1-n, n):
-            if 1-n < i < n-1 and 1-n < j < n-1:
-                continue
 
-            ni, nj = pos[0] + i, pos[1] + j
-            if 0 <= ni < N and 0 <= nj < M and test_case[ni][nj] == 0:
-                if can_go(ni, nj):
-                    test_case[ni][nj] = 2
-                    ans += 1
-                else:
-                    continue
+def run(pos, n=-1):
+    global ans, FLAG
 
-    if n < max([N, M]):
-        run(pos, n+1)
+    i, j = pos[0], pos[1]
+    flag = False
+    if test_case[i][j] == 0:
+        test_case[i][j] = n
+        ans += 1
+
+    for t in range(1, 5):
+        next_pos = go_next(pos, -t)
+        if next_pos:
+            flag = True
+            run(next_pos, n-1)
+            if FLAG:
+                return
     else:
-        return
+        if not flag:
+            back_pos = go_back(pos)
+            if back_pos:
+                run(back_pos, n)
+            else:
+                FLAG = True
+                return
 
 
 N, M = map(int,input().split())
 pos = list(map(int, input().split()))
 test_case = [list(map(int, input().split())) for _ in range(N)]
 
-test_case[pos[0]][pos[1]] = 2
-ans = 1
+ans = 0
+FLAG = False
 
 run(pos)
 
