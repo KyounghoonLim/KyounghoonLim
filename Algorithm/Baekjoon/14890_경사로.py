91fe11c4a0780b6b46e225b4f4774bf191aa6932
LKH <mogies@naver.com>
2022-07-04 17:40:14 +0900

    [Algo]: Baekjoon 14890_경사로 solved

--- Algorithm/Baekjoon/14890_경사로.py
+++ Algorithm/Baekjoon/14890_경사로.py
@@ -1,97 +1,42 @@
-import pprint
+def return_road(target, n):
+    for tar in target:
+        if visited[tar[0]][tar[1]] == n:
+            visited[tar[0]][tar[1]] = 0
 
 
-def is_continuous(target_load, temp=None):
-    if temp:
-        for t in range(N-1):
-            if target_load[t] != target_load[t+1]:
-                if temp[t] < L:
-                    return False
-    else:
-        target = target_load.index(min(target_load))
-        if target > len(target_load) - L:
+def make_road(road, n):
+    global ans
+
+    temp = [1] * N
+    for k in range(1, N):
+        result = abs(test_case[road[k][0]][road[k][1]] - test_case[road[k-1][0]][road[k-1][1]])
+        if result > 1:  ### 높이 검증 ###
             return False
-        for t in range(L):
-            if target_load[target + t] != target_load[target]:
-                return False
+        elif result == 0:   ### 연속성 체크 ###
+            temp[k] += temp[k-1]
+            for l in range(k-1, k-temp[k], -1):
+                temp[l] = temp[k]
 
-    return True
-
-
-def can_make_load_h(i):
-    temp = [1] * N
-    for j in range(N-L):
-        for k in range(1, L+1):
-            if test_case[i][j + k - 1] == test_case[i][j + k] and temp[j + k] == 1:
-                temp[j + k] = temp[j + k - 1] + 1
-            if abs(test_case[i][j] - test_case[i][j + k]) > 1:  # 높이 체크
-                return False
-            elif visited[i][j + k]: # 경사로 존재 여부 체크
-                return False
-    target_load = [test_case[i][t] for t in range(N)]
-    for t in range(N-1, 0, -1):
-        for u in range(t, -1, -1):
-            if target_load[t] == target_load[u]:
-                temp[u] = temp[t]
-            else:
-                break
-    if not is_continuous(target_load, temp):  # 연속성 체크
-        return False
-
-    return True
-
-
-def can_make_load_v(i):
-    temp = [1] * N
-    for j in range(N-L):
-        for k in range(1, L+1):
-            if test_case[j + k - 1][i] == test_case[j + k][i] and temp[j + k] == 1:
-                temp[j + k] = temp[j + k - 1] + 1
-            if abs(test_case[j][i] - test_case[j + k][i]) > 1:  # 높이 체크
-                return False
-            elif visited[j + k][i]: # 경사로 존재 여부 체크
-                return False
-    target_load = [test_case[t][i] for t in range(N)]
-    for t in range(N - 1, 0, -1):
-        for u in range(t, -1, -1):
-            if target_load[t] == target_load[u]:
-                temp[u] = temp[t]
-    if not is_continuous(target_load, temp):  # 연속성 체크
-        return False
-
-    return True
-
-
-def make_load_h():
-    global ans
-
-    for i in range(N):
-        if not can_make_load_h(i):
+    for t in range(N-1):  ### 연속성 검증 ###
+        result = test_case[road[t][0]][road[t][1]] - test_case[road[t+1][0]][road[t+1][1]]
+        if result == 0:
             continue
-        print('h', i)
-        for j in range(N - L):
-            temp_load = [test_case[i][j + t] for t in range(L+1)]
-            if test_case[i][j] == test_case[i][j+L] or not is_continuous(temp_load):
-                continue
-            for k in range(L):
-                visited[i][j+k] = 1
-        ans += 1
+        elif result == 1 and temp[t+1] >= L:  ### 오른쪽 검증 ###
+            for tar in range(t+1, t+L+1):
+                if tar >= N or visited[road[tar][0]][road[tar][1]] == n:    ### 길이 검증 및 경사로 유무 검증 ###
+                    return return_road(road, n)
+                else:
+                    visited[road[tar][0]][road[tar][1]] = n ### 경사로 쌓기 ###
+        elif result == -1 and temp[t] >= L: ### 왼쪽 검증 ###
+            for tar in range(t, t-L, -1):
+                if tar < 0 or visited[road[tar][0]][road[tar][1]] == n:    ### 길이 검증 및 경사로 유무 검증 ###
+                    return return_road(road, n)
+                else:
+                    visited[road[tar][0]][road[tar][1]] = n ### 경사로 쌓기 ###
+        else:
+            return False
 
-
-def make_load_v():
-    global ans
-
-    for i in range(N):
-        if not can_make_load_v(i):
-            continue
-        print('v', i)
-        for j in range(N - L):
-            temp_load = [test_case[j + t][i] for t in range(L + 1)]
-            if test_case[j][i] == test_case[j + L][i] or not is_continuous(temp_load):
-                continue
-            for k in range(L):
-                visited[j + k][i] = 1
-        ans += 1
+    ans += 1    ### 모든 검증 통과시 ###
 
 
 N, L = map(int, input().split())
@@ -100,8 +45,12 @@ visited = [[0] * N for _ in range(N)]
 
 ans = 0
 
-make_load_h()
-make_load_v()
+for i in range(N):
+    h_road, v_road = [], []
+    for j in range(N):
+        h_road.append([i, j])
+        v_road.append([j, i])
+    make_road(h_road, f'h-{i + 1}')
+    make_road(v_road, f'v-{i + 1}')
 
 print(ans)
-pprint.pprint(visited)
